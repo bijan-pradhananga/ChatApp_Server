0eef9d68f084ef14da04d2e8ffae701ca405ac04
Bijan <bijan2059@gmail.com>
2024-09-11 14:24:45 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const cors = require("cors");
+const {Server} = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+
+app.use(cors());
+const io = new Server(server,{
+    cors:{
+        origin:'http://localhost:5173',
+        methods:["GET","POST"]
+    }
+});
+
+server.listen(3001, () => console.log(`Server listening to port`, 3001));
+
+io.on('connection',(socket)=>{
+    console.log('New user connection id',socket.id);
+    socket.on("join_room",(data)=>{
+        socket.join(data);
+        console.log(`User ID ${socket.id} joined room ${data}`); 
+    })
+    socket.on("send_msg",(data)=>{
+        socket.to(data.room).emit("receive_msg",data)
+    })
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",socket.id)
+    })
+})
